1c89a39ec8aa405b4bdf198947fa67491becc6d5
jostunna <joelmubirru@gmail.com>
2023-04-13 22:18:50 +0300

    Create squares based on prompt size

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const gridContainer = document.getElementById("grid-container");
 const createGridButton = document.getElementById("create-grid");
-const square = document.createElement("div");
-square.classList.add("square");
-gridContainer.appendChild(square);
 
 createGridButton.addEventListener("click", () => {
   let size = +prompt("Enter number of squares");
-  console.log(Number.isInteger(size));
+  if (size > 0 && size <= 100) {
+    createGrid(size);
+  } else {
+    alert("Please enter a valid number between 1 and 100");
+  }
 });
+
+function createGrid(size) {
+  // Remove existing squares
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  // Create new squares
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+  }
+}
