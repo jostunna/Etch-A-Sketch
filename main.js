cab7fbfd67eb238c6da565c9c28d3de7d4664f34
jostunna <joelmubirru@gmail.com>
2023-04-13 22:06:36 +0300

    Create prompt for number of squares

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const gridContainer = document.getElementById("grid-container");
+const createGridButton = document.getElementById("create-grid");
 const square = document.createElement("div");
 square.classList.add("square");
 gridContainer.appendChild(square);
+
+createGridButton.addEventListener("click", () => {
+  let size = +prompt("Enter number of squares");
+  console.log(Number.isInteger(size));
+});
